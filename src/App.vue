b7f94fe2ca96732502f5e6dbf215d21be7b7229b
Lynnn <lynn_huang@cmh.com.tw>
2025-01-08 15:34:34 +0800

    fix: 修復main 定位

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="dashboard">
         <SideBar />
-        <main>
-            <router-view v-slot="{ Component }">
-                <transition
-                    name="slide-fade"
-                    mode="out-in"
-                >
-                    <div :key="route.name">
-                        <component :is="Component" />
-                    </div>
-                </transition>
-            </router-view>
-        </main>
+        <router-view v-slot="{ Component }">
+            <transition
+                name="slide-fade"
+                mode="out-in"
+            >
+                <main :key="route.name">
+                    <component :is="Component" />
+                </main>
+            </transition>
+        </router-view>
 
     </div>
 </template>
