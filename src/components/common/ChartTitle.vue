2a0c58c52ee470b87ebacfa816cdad20edf28448
Lynnn <lynn_huang@cmh.com.tw>
2025-01-24 10:10:56 +0800

    fix: 圖表標題ui調整

--- src/components/common/ChartTitle.vue
+++ src/components/common/ChartTitle.vue
@@ -21,19 +21,27 @@ const { title, type } = defineProps([
     font-size: 1rem;
     color: var(--title-color);
 
+    @media (max-width: 1500px) {
+        padding-left: 7px;
+    }
+
     &::before {
         content: "";
         position: absolute;
-        top: 4px;
+        top: 15%;
         left: 0;
         width: 3px;
-        height: 14px;
+        height: 70%;
         border-radius: 10px;
         background: var(--color-yellow);
+
+        @media (max-width: 1500px) {
+            width: 2px;
+        }
     }
 
     &.plan::before {
-      background: var(--color-deep-blue);
+		background: var(--color-deep-blue);
     }
 }
 </style>
